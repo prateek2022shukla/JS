56e2edc7a1be868c8d87bcd9dbece97ab2014091
Your Nameclear <you@example.com>
2022-02-05 15:49:58 +0530

    Task 67 Else if block

--- src/script.js
+++ src/script.js
@@ -576,3 +576,22 @@ function testElse(val) {
 }
 
 testElse(4);
+
+//Task 67
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+
+  else{
+    return "Between 5 and 10";
+  }
+
+  
+}
+
+testElseIf(7);

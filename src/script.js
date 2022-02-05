7b0cff0af2e97970f74b1db9aac6b361b4c225dd
Your Nameclear <you@example.com>
2022-02-05 15:48:00 +0530

    Task 66 Else block

--- src/script.js
+++ src/script.js
@@ -556,3 +556,23 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+//Task 66
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);

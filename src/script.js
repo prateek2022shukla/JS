92c0c49b492c5cc92f17128f5c00311713c03580
Your Nameclear <you@example.com>
2022-02-05 15:30:00 +0530

    Task 61 Comparision Operator >=

--- src/script.js
+++ src/script.js
@@ -463,3 +463,34 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+
+//Task 60
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+//Task 61
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);

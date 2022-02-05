2ff84c6ebd5d6c4feb22eba1cc795e99df8cb031
Your Nameclear <you@example.com>
2022-02-05 13:36:41 +0530

    Task 47 Local Scope and Fucntion

--- src/script.js
+++ src/script.js
@@ -283,31 +283,38 @@ let myGlobal;
 myGlobal = 10;
 
 function fun1() {
-  // Assign 5 to oopsGlobal Here
+ 
    oopsGlobal = 5;
 }
 
-// Only change code above this line
-
-// Declare the myGlobal variable below this line
-let myGlobal;
-myGlobal = 10;
+let myGlobal2;
+myGlobal2 = 10;
 
 function fun1() {
-  // Assign 5 to oopsGlobal Here
+ 
    oopsGlobal = 5;
 }
 
-// Only change code above this line
 
 function fun2() {
   var output = "";
-  if (typeof myGlobal != "undefined") {
+  if (typeof myGlobal2 != "undefined") {
     output += "myGlobal: " + myGlobal;
   }
-  if (typeof oopsGlobal != "undefined") {
+  if (typeof oopsGlobal2 != "undefined") {
     output += " oopsGlobal: " + oopsGlobal;
   }
   console.log(output);
 }
  
+
+
+//Task 47 
+
+function myLocalScope() {
+    var myVar;
+  
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  console.log('outside myLocalScope', myVar);

5772faae2690fb0bf5ef5f5ade976062a08748a4
Your Nameclear <you@example.com>
2022-02-05 15:01:19 +0530

    Task 51 Stand in Line

--- src/script.js
+++ src/script.js
@@ -355,3 +355,15 @@ function processArg(num) {
 }
 
 processed = processArg(7);
+
+//Task 51
+function nextInLine(arr, item) {
+
+  arr.push(item);  
+  return arr.shift();  
+}
+
+const testArr = [1, 2, 3, 4, 5];
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));

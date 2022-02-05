7cb0dd328395dd2396d06017294813508570f847
Your Nameclear <you@example.com>
2022-02-05 15:37:55 +0530

    Task 63 Less or equal to operator

--- src/script.js
+++ src/script.js
@@ -495,7 +495,7 @@ function testGreaterOrEqual(val) {
 
 testGreaterOrEqual(10);
 
-//Task 61
+//Task 62
 function testLessThan(val) {
   if (val<25) {  // Change this line
     return "Under 25";
@@ -509,3 +509,18 @@ function testLessThan(val) {
 }
 
 testLessThan(10);
+
+//Task 63
+function testLessOrEqual(val) {
+  if (val<=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);

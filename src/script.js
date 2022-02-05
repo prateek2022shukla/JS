5f183fb6330e6956bdc4ea7577b12f556c73d343
Your Nameclear <you@example.com>
2022-02-05 15:22:40 +0530

    Task 58 Not Equal to with type operator !==

--- src/script.js
+++ src/script.js
@@ -436,3 +436,14 @@ function testNotEqual(val) {
 }
 
 testNotEqual(10);
+
+//Task 58 
+// Setup
+function testStrictNotEqual(val) {
+  if (val!==17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
